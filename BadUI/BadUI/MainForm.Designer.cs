8ed460f99a488921e6da51ebaae17744823d8c48
HumanEquivalentUnit <HumanEquivalentUnit@users.noreply.github.com>
2020-03-29 22:20:12 +0100

    Initial commit

---
+++ BadUI/BadUI/MainForm.Designer.cs
@@ -0,0 +1,383 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Si
+ * Date: 27/03/2020
+ * Time: 09:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace BadUI
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton20;
+		private System.Windows.Forms.RadioButton radioButton19;
+		private System.Windows.Forms.RadioButton radioButton18;
+		private System.Windows.Forms.RadioButton radioButton17;
+		private System.Windows.Forms.RadioButton radioButton16;
+		private System.Windows.Forms.RadioButton radioButton15;
+		private System.Windows.Forms.RadioButton radioButton14;
+		private System.Windows.Forms.RadioButton radioButton13;
+		private System.Windows.Forms.RadioButton radioButton12;
+		private System.Windows.Forms.RadioButton radioButton11;
+		private System.Windows.Forms.RadioButton radioButton10;
+		private System.Windows.Forms.RadioButton radioButton9;
+		private System.Windows.Forms.RadioButton radioButton8;
+		private System.Windows.Forms.RadioButton radioButton7;
+		private System.Windows.Forms.RadioButton radioButton6;
+		private System.Windows.Forms.RadioButton radioButton5;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.ToolTip toolTip1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton20 = new System.Windows.Forms.RadioButton();
+			this.radioButton19 = new System.Windows.Forms.RadioButton();
+			this.radioButton18 = new System.Windows.Forms.RadioButton();
+			this.radioButton17 = new System.Windows.Forms.RadioButton();
+			this.radioButton16 = new System.Windows.Forms.RadioButton();
+			this.radioButton15 = new System.Windows.Forms.RadioButton();
+			this.radioButton14 = new System.Windows.Forms.RadioButton();
+			this.radioButton13 = new System.Windows.Forms.RadioButton();
+			this.radioButton12 = new System.Windows.Forms.RadioButton();
+			this.radioButton11 = new System.Windows.Forms.RadioButton();
+			this.radioButton10 = new System.Windows.Forms.RadioButton();
+			this.radioButton9 = new System.Windows.Forms.RadioButton();
+			this.radioButton8 = new System.Windows.Forms.RadioButton();
+			this.radioButton7 = new System.Windows.Forms.RadioButton();
+			this.radioButton6 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(9, 28);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(262, 24);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "UNIX Epoch";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(6, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(271, 28);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Please select your birthday:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(471, 397);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(95, 40);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "&Continue";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(6, 373);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(95, 40);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Ne&xt Page";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(107, 373);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(95, 40);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Bac&k";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton20);
+			this.groupBox1.Controls.Add(this.radioButton19);
+			this.groupBox1.Controls.Add(this.radioButton18);
+			this.groupBox1.Controls.Add(this.radioButton17);
+			this.groupBox1.Controls.Add(this.radioButton16);
+			this.groupBox1.Controls.Add(this.radioButton15);
+			this.groupBox1.Controls.Add(this.radioButton14);
+			this.groupBox1.Controls.Add(this.radioButton13);
+			this.groupBox1.Controls.Add(this.radioButton12);
+			this.groupBox1.Controls.Add(this.radioButton11);
+			this.groupBox1.Controls.Add(this.radioButton10);
+			this.groupBox1.Controls.Add(this.radioButton9);
+			this.groupBox1.Controls.Add(this.radioButton8);
+			this.groupBox1.Controls.Add(this.radioButton7);
+			this.groupBox1.Controls.Add(this.radioButton6);
+			this.groupBox1.Controls.Add(this.radioButton5);
+			this.groupBox1.Controls.Add(this.radioButton4);
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Location = new System.Drawing.Point(6, 40);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(560, 350);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "On this day..";
+			// 
+			// radioButton20
+			// 
+			this.radioButton20.Location = new System.Drawing.Point(291, 298);
+			this.radioButton20.Name = "radioButton20";
+			this.radioButton20.Size = new System.Drawing.Size(262, 33);
+			this.radioButton20.TabIndex = 23;
+			this.radioButton20.TabStop = true;
+			this.radioButton20.Text = "Yesterday (lol!)";
+			this.radioButton20.UseVisualStyleBackColor = true;
+			// 
+			// radioButton19
+			// 
+			this.radioButton19.Location = new System.Drawing.Point(9, 144);
+			this.radioButton19.Name = "radioButton19";
+			this.radioButton19.Size = new System.Drawing.Size(262, 33);
+			this.radioButton19.TabIndex = 22;
+			this.radioButton19.TabStop = true;
+			this.radioButton19.Text = "\"Programming in COBOL: A Course of Twelve Television Lectures\" published";
+			this.radioButton19.UseVisualStyleBackColor = true;
+			// 
+			// radioButton18
+			// 
+			this.radioButton18.Location = new System.Drawing.Point(9, 302);
+			this.radioButton18.Name = "radioButton18";
+			this.radioButton18.Size = new System.Drawing.Size(262, 24);
+			this.radioButton18.TabIndex = 21;
+			this.radioButton18.TabStop = true;
+			this.radioButton18.Text = "February 29th 2005";
+			this.radioButton18.UseVisualStyleBackColor = true;
+			// 
+			// radioButton17
+			// 
+			this.radioButton17.Location = new System.Drawing.Point(292, 238);
+			this.radioButton17.Name = "radioButton17";
+			this.radioButton17.Size = new System.Drawing.Size(262, 24);
+			this.radioButton17.TabIndex = 20;
+			this.radioButton17.TabStop = true;
+			this.radioButton17.Text = "Neil Armstrong landed back on Earth";
+			this.radioButton17.UseVisualStyleBackColor = true;
+			// 
+			// radioButton16
+			// 
+			this.radioButton16.Location = new System.Drawing.Point(9, 268);
+			this.radioButton16.Name = "radioButton16";
+			this.radioButton16.Size = new System.Drawing.Size(262, 24);
+			this.radioButton16.TabIndex = 19;
+			this.radioButton16.TabStop = true;
+			this.radioButton16.Text = "Palindromic date sums to 8";
+			this.radioButton16.UseVisualStyleBackColor = true;
+			// 
+			// radioButton15
+			// 
+			this.radioButton15.Location = new System.Drawing.Point(292, 208);
+			this.radioButton15.Name = "radioButton15";
+			this.radioButton15.Size = new System.Drawing.Size(262, 24);
+			this.radioButton15.TabIndex = 18;
+			this.radioButton15.TabStop = true;
+			this.radioButton15.Text = "My dog had 3 puppies :D :D";
+			this.radioButton15.UseVisualStyleBackColor = true;
+			// 
+			// radioButton14
+			// 
+			this.radioButton14.Location = new System.Drawing.Point(9, 58);
+			this.radioButton14.Name = "radioButton14";
+			this.radioButton14.Size = new System.Drawing.Size(262, 24);
+			this.radioButton14.TabIndex = 17;
+			this.radioButton14.TabStop = true;
+			this.radioButton14.Text = "\'Despacito\' released";
+			this.radioButton14.UseVisualStyleBackColor = true;
+			this.radioButton14.CheckedChanged += new System.EventHandler(this.RadioButton14CheckedChanged);
+			// 
+			// radioButton13
+			// 
+			this.radioButton13.Location = new System.Drawing.Point(9, 208);
+			this.radioButton13.Name = "radioButton13";
+			this.radioButton13.Size = new System.Drawing.Size(262, 24);
+			this.radioButton13.TabIndex = 16;
+			this.radioButton13.TabStop = true;
+			this.radioButton13.Text = "EventNotFoundException in line 30 org.Jav..";
+			this.radioButton13.UseVisualStyleBackColor = true;
+			// 
+			// radioButton12
+			// 
+			this.radioButton12.Location = new System.Drawing.Point(292, 178);
+			this.radioButton12.Name = "radioButton12";
+			this.radioButton12.Size = new System.Drawing.Size(262, 24);
+			this.radioButton12.TabIndex = 15;
+			this.radioButton12.TabStop = true;
+			this.radioButton12.Text = "JFK announces moon mission";
+			this.radioButton12.UseVisualStyleBackColor = true;
+			// 
+			// radioButton11
+			// 
+			this.radioButton11.Location = new System.Drawing.Point(9, 178);
+			this.radioButton11.Name = "radioButton11";
+			this.radioButton11.Size = new System.Drawing.Size(262, 24);
+			this.radioButton11.TabIndex = 14;
+			this.radioButton11.TabStop = true;
+			this.radioButton11.Text = "избрал михаила горбачева";
+			this.radioButton11.UseVisualStyleBackColor = true;
+			// 
+			// radioButton10
+			// 
+			this.radioButton10.Location = new System.Drawing.Point(291, 268);
+			this.radioButton10.Name = "radioButton10";
+			this.radioButton10.Size = new System.Drawing.Size(262, 24);
+			this.radioButton10.TabIndex = 13;
+			this.radioButton10.TabStop = true;
+			this.radioButton10.Text = "The Mayflower arrived in America";
+			this.radioButton10.UseVisualStyleBackColor = true;
+			// 
+			// radioButton9
+			// 
+			this.radioButton9.Location = new System.Drawing.Point(292, 148);
+			this.radioButton9.Name = "radioButton9";
+			this.radioButton9.Size = new System.Drawing.Size(262, 24);
+			this.radioButton9.TabIndex = 12;
+			this.radioButton9.TabStop = true;
+			this.radioButton9.Text = "Elvis passed away";
+			this.radioButton9.UseVisualStyleBackColor = true;
+			// 
+			// radioButton8
+			// 
+			this.radioButton8.Location = new System.Drawing.Point(292, 118);
+			this.radioButton8.Name = "radioButton8";
+			this.radioButton8.Size = new System.Drawing.Size(262, 24);
+			this.radioButton8.TabIndex = 11;
+			this.radioButton8.TabStop = true;
+			this.radioButton8.Text = "Windows NT Epoch";
+			this.radioButton8.UseVisualStyleBackColor = true;
+			// 
+			// radioButton7
+			// 
+			this.radioButton7.Location = new System.Drawing.Point(9, 118);
+			this.radioButton7.Name = "radioButton7";
+			this.radioButton7.Size = new System.Drawing.Size(262, 24);
+			this.radioButton7.TabIndex = 10;
+			this.radioButton7.TabStop = true;
+			this.radioButton7.Text = "Premiere episode of Judge Judy";
+			this.radioButton7.UseVisualStyleBackColor = true;
+			// 
+			// radioButton6
+			// 
+			this.radioButton6.Location = new System.Drawing.Point(292, 88);
+			this.radioButton6.Name = "radioButton6";
+			this.radioButton6.Size = new System.Drawing.Size(262, 24);
+			this.radioButton6.TabIndex = 9;
+			this.radioButton6.TabStop = true;
+			this.radioButton6.Text = "Last Solar Eclipse visible in Bangladesh in C20th";
+			this.radioButton6.UseVisualStyleBackColor = true;
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.Location = new System.Drawing.Point(9, 88);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(262, 24);
+			this.radioButton5.TabIndex = 8;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "Dr Heimlich announced his maneouvre";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.Location = new System.Drawing.Point(292, 58);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(262, 24);
+			this.radioButton4.TabIndex = 7;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "The Beatles break up";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(9, 238);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(262, 24);
+			this.radioButton3.TabIndex = 6;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Janet Reno is sworn in as attorney general";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(292, 28);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(262, 24);
+			this.radioButton2.TabIndex = 5;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Two Boeing 747s collide at Tenerife Airport";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// toolTip1
+			// 
+			this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.ToolTip1Popup);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(571, 444);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "BadUI";
+			this.groupBox1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ BadUI/BadUI/MainForm.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Si
+ * Date: 27/03/2020
+ * Time: 09:11
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BadUI
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+			toolTip1 = new ToolTip();
+			toolTip1.SetToolTip(this.radioButton3, "12th May 1977");
+			toolTip1.SetToolTip(this.radioButton14, "12th Jan 2017");
+			toolTip1.SetToolTip(this.radioButton9, "ELVIS LIVES!!");
+			
+		}
+		void RadioButton14CheckedChanged(object sender, EventArgs e)
+		{
+			MessageBox.Show("oi oi");
+		}
+		
+		void ToolTip1Popup(object sender, PopupEventArgs e)
+		{
+	
+		}
+		void RadioButton3CheckedChanged(object sender, EventArgs e)
+		{
+			MessageBox.Show("Error, birthday must be on the approved date list", "No.", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+		}
+	}
+}
